4ee46b67f8a0d25238807f9e694f3677c39ad0f4
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-07-31 13:54:11 +0300

    Add project files.

---
+++ Casgem_Ajax/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=CasgemAjaxDb;integrated security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
